da8f8d8b10a2074008d25b8e698e9025b72df690
Moskva_Voronezh <roman_moskvin_97@mail.ru>
2021-05-20 23:05:31 +0700

    app init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" v-if="isInit">
+    <router-view />
+  </div>
+</template>
+
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component({})
+export default class App extends Vue {
+  isInit = false;
+  async created() {
+    this.isInit = true;
+  }
+ }
+</script>
+
+<style lang="scss">
+
+</style>
