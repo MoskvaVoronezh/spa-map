8f3632aca0e499d3bf410ffed3553c1dffc5cd59
Moskva_Voronezh <roman_moskvin_97@mail.ru>
2021-05-22 15:56:01 +0700

    added home page

---
+++ src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    home
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from "vue-property-decorator";
+
+  @Component({})
+  export default class HomePage extends Vue {
+    created() {
+      console.log('created');
+    }
+  }
+</script>
+
+<style lang="scss">
+  
+</style>
