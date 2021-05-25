4dd63b71a57549fb63b2630267d6c17d7c75e6d3
Moskva_Voronezh <roman_moskvin_97@mail.ru>
2021-05-25 21:32:14 +0700

    fixed spaces

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,10 +3,7 @@
 </template>
 
 <script lang="ts">
-	import {
-		Component,
-		Vue
-	} from "vue-property-decorator";
+	import { Component, Vue } from "vue-property-decorator";
 	declare var ymaps: any;
 	@Component({})
 	export default class Map extends Vue {
